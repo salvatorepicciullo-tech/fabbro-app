9f85f6ba50c553b36e8d5480c5d1e216a4564416
salvatorepicciullo-tech <salvatore.picciullo@gmail.com>
2026-05-05 22:29:50 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -187,6 +187,8 @@ app.delete("/quotes/:id", async (req, res) => {
    SERVER
 ========================= */
 
-app.listen(3001, () => {
-  console.log("🚀 Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
